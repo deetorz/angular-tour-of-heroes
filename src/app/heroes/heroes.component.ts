2bf874a1ff801af6fc78062bd74599fb050ae100
Torres <derek.torres@p86592.intra.rakuten.co.jp>
2022-01-12 15:56:38 +0900

    cleaned up heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from '../hero';
 import { HeroService } from '../services/hero.service';
-import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,24 +8,16 @@ import { MessageService } from '../services/message.service';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero?: Hero;
   heroes: Hero[] = [];
-  
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
-  
+
+  constructor(private heroService: HeroService) { }
+
   ngOnInit(): void {
     this.getHeroes();
   }
-  
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`You selected ${hero.name}!`);
-  }
-  
+
   getHeroes(): void {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
-    this.selectedHero = this.heroes[0]
   }
 }
