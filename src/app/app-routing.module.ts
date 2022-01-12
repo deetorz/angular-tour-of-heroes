15442cf6014c893b6a584593d20b329e83558576
Torres <derek.torres@p86592.intra.rakuten.co.jp>
2022-01-12 15:43:23 +0900

    removing her details from main app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
