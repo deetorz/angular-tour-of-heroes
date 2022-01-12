a5605b4711fc6f9d40f3a094929b4faeb10b9716
Torres <derek.torres@p86592.intra.rakuten.co.jp>
2022-01-12 16:20:44 +0900

    added gethero function to hero service

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../services/hero.service';
 
 @Component({
   selector: 'app-hero-details',
@@ -9,9 +12,20 @@ import { Hero } from '../hero';
 export class HeroDetailsComponent implements OnInit {
   @Input() hero?: Hero;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private heroService: HeroService,
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
 }
